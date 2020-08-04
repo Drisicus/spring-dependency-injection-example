a8f888207a32f7d4a7f4d47abfc363dbf1dff2b8
Andrés <aaasssccc@gmail.com>
2020-08-04 18:58:49 +0200

    Profiles for different primary service implementation

--- src/main/java/es/springframework/springdependencyinjectionexample/services/PrimarySpanishGreetingsService.java
+++ src/main/java/es/springframework/springdependencyinjectionexample/services/PrimarySpanishGreetingsService.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+@Service
+@Profile("es")
+@Primary
+public class PrimarySpanishGreetingsService implements GreetingService{
 
-@Service // Spring makes it a bean
-@Primary // If several beans, this is default
-@Profile("en")
-public class PrimaryGreetingsService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Primary Greetings Service --> Hello!";
+        return "Servicio primario de saludo en español";
     }
 }
