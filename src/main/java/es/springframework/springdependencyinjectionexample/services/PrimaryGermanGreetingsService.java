049a731e46b8d9bbe05f9ee77f8dd291b5cf9014
Andrés <aaasssccc@gmail.com>
2020-08-04 19:04:59 +0200

    Default profile

--- src/main/java/es/springframework/springdependencyinjectionexample/services/PrimaryGermanGreetingsService.java
+++ src/main/java/es/springframework/springdependencyinjectionexample/services/PrimaryGermanGreetingsService.java
@@ -4,13 +4,12 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-
-@Service // Spring makes it a bean
-@Primary // If several beans, this is default
-@Profile({"en", "default"})
-public class PrimaryGreetingsService implements GreetingService {
+@Service
+@Profile("de")
+@Primary
+public class PrimaryGermanGreetingsService implements GreetingService{
     @Override
     public String sayGreeting() {
-        return "Primary Greetings Service --> Hello!";
+        return "German Service";
     }
 }
