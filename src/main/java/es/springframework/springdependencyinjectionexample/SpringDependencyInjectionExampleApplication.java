b14662f1be453f9c5a6bda1dbbd9a5d8c056f567
pablos16 <pablos16@ucm.es>
2020-08-12 18:24:40 +0200

    Add another properties file and use multiple PropertySources

--- src/main/java/es/springframework/springdependencyinjectionexample/SpringDependencyInjectionExampleApplication.java
+++ src/main/java/es/springframework/springdependencyinjectionexample/SpringDependencyInjectionExampleApplication.java
@@ -4,6 +4,7 @@ import es.springframework.springdependencyinjectionexample.controllers.Construct
 import es.springframework.springdependencyinjectionexample.controllers.MyController;
 import es.springframework.springdependencyinjectionexample.controllers.PropertyInjectedController;
 import es.springframework.springdependencyinjectionexample.controllers.SetterInjectedController;
+import es.springframework.springdependencyinjectionexample.exampleBeans.FakeBroker;
 import es.springframework.springdependencyinjectionexample.exampleBeans.FakeDataSource;
 
 import org.springframework.boot.SpringApplication;
@@ -26,5 +27,11 @@ public class SpringDependencyInjectionExampleApplication {
         System.out.println(fakeDataSource.getUser());
         System.out.println(fakeDataSource.getPassword());
         System.out.println(fakeDataSource.getUrl());
+
+        FakeBroker fakeBroker = context.getBean(FakeBroker.class);
+        System.out.println(fakeBroker.getUser());
+        System.out.println(fakeBroker.getPassword());
+        System.out.println(fakeBroker.getUrl());
+
     }
 }
